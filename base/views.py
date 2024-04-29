7b20a31b8191c47416ed4929f405198fda7b514e
satisprj <parajulisatish0@gmail.com>
2024-04-29 23:56:59 +0545

    search bar

--- base/views.py
+++ base/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from .models import Room
+from django.db.models import Q
+from .models import Room,Topic
 from .forms import RoomForm
 # Create your views here.
 
 def home(request):
-    rooms=Room.objects.all()
-    contex={'rooms':rooms}
+    q=request.GET.get('q') if request.GET.get('q')!=None else ''
+
+    rooms=Room.objects.filter(Q(topic__name__icontains=q),
+    Q(name__icontains=q)|
+    Q(description__icontains=q))
+    topic=Topic.objects.all() 
+    contex={'rooms':rooms,'topic':topic}
 
     return render(request,'base/home.html',contex)
 
 def room(request,pk):
     room=Room.objects.get(id=pk)
+    
     contex={'room':room}
     return render(request,'base/room.html',contex)
 
